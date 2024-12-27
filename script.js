90cc8a2464fa113d238dcfa5a113dd9d5033baed
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-27 12:15:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+//your JS code here. If required.
+const panels = document.querySelectorAll(".panel");
+let currentActive= panels[0];
+
+panels.forEach(panel => {
+	panel.addEventListener("click", () => {
+		if(currentActive !== panel) {
+			panel.classList.add("active");
+			currentActive.classList.remove("active");
+			currentActive= panel;
+		}
+	})
+})
